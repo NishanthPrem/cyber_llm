bc87e8b1c0e26c48fd33a874c43cf81d58e8b1cb
Nishanth Prem <nishanthprem8@gmail.com>
2025-04-01 15:10:44 -0500

    Adding logfire and pydantic_ai

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from cyber-llm!")
+import logfire
+import os
+import openai
+from pydantic_ai import Agent
+from dotenv import load_dotenv
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+logfire.configure()
+logfire.instrument_openai()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+agent = Agent('openai:gpt-4o')
+
+result = agent.run_sync(
+    'How does pyodide let you run Python in the browser? (short answer please)'
+)
+
+print(f'Output: {result.data}')
